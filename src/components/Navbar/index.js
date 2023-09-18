23f932c3e99aba736972378543216da6e964a1ba
Alo Timothy <59998792+SonOfUri@users.noreply.github.com>
2023-09-18 10:35:18 +0100

    Update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -11,9 +11,9 @@ const Navbar = () => {
     </div>
  
     <div class="links" >
-        <div><a href="https://tameShibridge.com/" target='_blank'>Home</a></div>
-        <div><a href="https://tameShibridge.com/#about" target='_blank'>About</a></div>
-        <div><a href="https://tameShibridge.com/#services" target='_blank'>Roadmap</a></div>
+        <div><a href="#" target='_blank'>Home</a></div>
+        <div><a href="#t" target='_blank'>About</a></div>
+        <div><a href="#" target='_blank'>Roadmap</a></div>
     </div>
 </nav>
 
