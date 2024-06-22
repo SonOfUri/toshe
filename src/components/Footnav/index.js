ca5c7c19c6ce032560322678d6813dc3e5bc0869
Alo Timothy <alotimothy01@gmail.com>
2024-06-22 10:25:12 +0100

    Update links

--- src/components/Footnav/index.js
+++ src/components/Footnav/index.js
@@ -4,26 +4,25 @@ import logo from '../../intelai.png';
 
 const Footnav = () => {
   return (
-     
-    <div class='footer'>
-        <div class='footer-img'>
-        <img id="logo2-image" src={logo} alt='Logo' />
-        </div>
-        <div class='footer-links'>
-          <a href='#' target='_blank'>
-        <i class='bx bxl-telegram'></i>
+    <div class="footer">
+      <div class="footer-img">
+        <img id="logo2-image" src={logo} alt="Logo" />
+      </div>
+      <div class="footer-links">
+        <a href="https://t.me/IntelAIerc" target="_blank">
+          <i class="bx bxl-telegram"></i>
+        </a>
 
-          </a>
+        <a href="https://x.com/intelaieth" target="_blank">
+          <i class="bx bxl-twitter"></i>
+        </a>
 
-          <a href='#' target='_blank'>
-       <i class='bx bxl-twitter' ></i>
-
-          </a>
-        </div>
-      
+        <a href="https://intel-ai-bot.gitbook.io/" target="_blank">
+          <i class="bx bxs-file-doc"></i>
+        </a>
+      </div>
     </div>
-
-  )
+  );
 }
 
 export default Footnav
